05d8127e16f8d2d9dcbd6b58473055ae6c267c9e
theloom <jeremy@j.local>
2017-07-20 11:27:51 -0600

    Add ability to call my API gateway and display faker images through a service.

--- giftube/src/app/gifs/gifs.component.ts
+++ giftube/src/app/gifs/gifs.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; import { Observable } from 'rxjs/Observable';
+import { GiftubeApiService } from '../giftube-api.service';
 
 @Component({
   selector: 'app-gifs',
   templateUrl: './gifs.component.html',
   styleUrls: ['./gifs.component.css']
 })
-export class GifsComponent implements OnInit {
 
-  constructor() { }
+export class GifsComponent implements OnInit {
+  gifs;
+
+  constructor(private _giftubeApiService: GiftubeApiService) { }
 
   ngOnInit() {
+    this._giftubeApiService.fetchGifs().subscribe(gifs => this.gifs = gifs,
+      error => console.log('Error fetching gifs'));
   }
-
 }
--- giftube/src/app/giftube-api.service.ts
+++ giftube/src/app/giftube-api.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable'; import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GiftubeApiService {
 
-  constructor() { }
+  baseUrl: string;
 
+  constructor(private http: Http) {
+    this.baseUrl = "https://0sm24gz11l.execute-api.us-east-1.amazonaws.com/testServerless"
+  }
+
+  fetchGifs(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/gifs`)
+      .map(response => response.json());
+  }
 }
